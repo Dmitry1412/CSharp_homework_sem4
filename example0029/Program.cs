ad49e88fee124ee73240b4e2148e2f27dd518bbe
Dmitry <sicily007@rambler.ru>
2023-08-27 00:59:47 +0300

    add_example29

---
+++ example0029/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+Задачу не понял, откуда берутся исходные данные
+*/
+PrintArray(GenerateArray(8));
+
+int[] GenerateArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0, 10);
+    }
+    return result;
+}
+
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array) // перебирает непосредственно значение элементов массива
+    {
+        Console.Write($"{i} ");
+    }
+}
